da00d35acdc3452f52c02c63a65b06e1fae02d66
Robson Peacock <robson_peacock20@outlook.com>
2020-08-02 15:43:36 +0100

    Implement SMS tool in lib

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,6 @@ Bundler.require(*Rails.groups)
 module TimeTracker
   class Application < Rails::Application
     config.load_defaults 6.0
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
